c8b56496125225e8093850588c6970c898fa8daf
Srijith <srijithka72@gmail.com>
2023-09-23 19:51:12 -0500

    My favs movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+    {:title => 'Top Gun Maverick', :rating => 'R',
+    :release_date => '13-Aug-2022'},
+    {:title => 'Harry Potter : Deathly Hollows', :rating => 'PG-13',
+    :release_date => '18-Aug-2020'}
 ]
 
 more_movies.each do |movie|
